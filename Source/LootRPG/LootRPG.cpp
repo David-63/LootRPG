52218657e0cb5e00f556d0e6b9bed60783d61581
David-63 <jiseop.63@gmail.com>
2024-07-10 01:08:54 +0900

    initialize project
    template third person shooter

---
+++ Source/LootRPG/LootRPG.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "LootRPG.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LootRPG, "LootRPG" );
+ 
